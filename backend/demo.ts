8874b02a5ee36583e7ae62fd24bc2057efbc369b
Axel Fernández <axl1210@estudiantec.cr>
2021-06-27 23:29:56 -0600

    ts transform

---
+++ backend/demo.ts
@@ -0,0 +1,21 @@
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log(`MongoDB db connection established successfully.`);
+});
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
