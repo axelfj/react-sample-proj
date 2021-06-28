3a56ea4d4ae69ad4811c383bbe72be07c6804150
Axel Fernández <azzefj98@gmail.com>
2021-06-28 00:59:43 -0600

    errorHandler init

---
+++ src/components/shared/errorHandler.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+interface IProps {
+    theFunction: void;
+}
+
+
+const errorsDict = {
+    "rx001": 957,
+    "rx002": 1002,
+    "rx003": 1002,
+}
+
+const errorHandler: React.FC<any> = (IProps) => {
+
+
+    return <>
+
+        </>
+}
+
+export default errorHandler;
